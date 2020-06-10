29e1db9691fead8d26cd139aaa988ff3bc758f75
Tukale <ridwan.tukale@outlook.com>
2020-06-10 14:57:26 +0300

    build

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/air/Desktop/proj/tuke/realvideo/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/air/Desktop/proj/tuke/realvideo/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/air/Desktop/proj/tuke/realvideo/src/pages/index.js")))
 }
