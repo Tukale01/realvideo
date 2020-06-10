9e2239810b2eca95bcb7928e809f49c73549aea4
Tukale <ridwan.tukale@outlook.com>
2020-06-10 14:51:34 +0300

    proj innit

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/Users/air/Desktop/proj/tuke/realvideo/node_modules/gatsby-plugin-web-font-loader/gatsby-browser.js'),
+      options: {"plugins":[],"google":{"families":["Merriweather:400,300,300italic,400italic,700,700italic","Merriweather+Sans:400,700"]}},
+    },{
+      plugin: require('/Users/air/Desktop/proj/tuke/realvideo/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ src/pages/index.js
@@ -0,0 +1,236 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Scroller from "../components/scroller"
+import PortfolioModal from "../components/portfolio/modal"
+import PortfolioCarousel from "../components/portfolio/carousel"
+
+export default class IndexPage extends React.Component {
+  constructor(props) {
+    super(props)
+    Scroller.handleAnchorScroll = Scroller.handleAnchorScroll.bind(this)
+    this.state = {
+      modalShow: false,
+      modalCurrent: 0
+    }
+    this.handlePortfolioClick = this.handlePortfolioClick.bind(this);
+    this.setModal = this.setModal.bind(this);
+  }
+
+  handlePortfolioClick(index, e) {
+    e.preventDefault();
+    this.setModal(true, index);
+  }
+
+  setModal(isShown, current) {
+    this.setState({
+      modalShow: isShown,
+      modalCurrent: current
+    });
+  }
+
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home"/>
+        <section className="page-section bg-primary" id="about">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-lg-8 text-center">
+                <h2 className="text-white mt-0">We've got what you need!</h2>
+                <hr className="divider light my-4"/>
+                <p className="text-white-50 mb-4">Our business minded team doesnt simply shoot and edit worldclass videos we consider
+                your brand identity, voice and reputation to create a tailored content that meets the expecttations 
+                of your desired audiences!</p>
+                <a className="btn btn-light btn-xl js-scroll-trigger" href="https://www.youtube.com/channel/UCEXIEByIYStcq1UdlCDFsKg"
+                   target='_blank' >View More of our Work!</a>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="page-section" id="services">
+          <div className="container">
+            <h2 className="text-center mt-0">At Your Service</h2>
+            <hr className="divider my-4"/>
+            <div className="row">
+              <div className="col-lg-3 col-md-6 text-center">
+                <div className="mt-5">
+                  <i className="fas fa-4x fa-gem text-primary mb-4"></i>
+                  <h3 className="h4 mb-2">Video Production</h3>
+                  <p className="text-muted mb-0">As creators working together,
+                   we here at Real Video Productions we have an undying passion to make
+                    something beautiful that truly stands out.!</p>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6 text-center">
+                <div className="mt-5">
+                  <i className="fas fa-4x fa-heart text-primary mb-4"></i>
+                  <h3 className="h4 mb-2">Creative Advertisements</h3>
+                  <p className="text-muted mb-0">Our efficient and economical advertising 
+                  system will allow you to present yourself to a wide audience and attract 
+                  potential visitors to your website & channels.</p>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6 text-center">
+                <div className="mt-5">
+                  <i className="fas fa-4x fa-globe text-primary mb-4"></i>
+                  <h3 className="h4 mb-2">Film & Video Management</h3>
+                  <p className="text-muted mb-0">We produce award winning short films with impact that attracts the your audiences
+                  and satisfy your viewers!</p>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6 text-center">
+                <div className="mt-5">
+                  <i className="fas fa-4x fa-laptop-code text-primary mb-4"></i>
+                  <h3 className="h4 mb-2">Web Development</h3>
+                  <p className="text-muted mb-0">We create web apps, services, 
+                  and sites that accurately answer your expectations 
+                  and satisfy the specific needs of your company. </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section id="portfolio">
+          <div className="container-fluid p-0">
+            <div className="row no-gutters">
+              <div className="col-lg-4 col-sm-6">
+                <a className="portfolio-box" href="img/portfolio/fullsize/1.jpg" onClick={this.handlePortfolioClick.bind(this, 0)}>
+                  <Img fluid={this.props.data.images.edges[0].node.childImageSharp.fluid}/>
+                  <div className="portfolio-box-caption">
+                    <div className="project-category text-white-50">
+                      Video Production
+                    </div>
+                    <div className="project-name">
+                      GEW - Shaqodoon
+                    </div>
+                  </div>
+                </a>
+              </div>
+              <div className="col-lg-4 col-sm-6">
+                <a className="portfolio-box" href="img/portfolio/fullsize/2.jpg" onClick={this.handlePortfolioClick.bind(this, 1)}>
+                  <Img fluid={this.props.data.images.edges[1].node.childImageSharp.fluid}/>
+                  <div className="portfolio-box-caption">
+                    <div className="project-category text-white-50">
+                      Video Production
+                    </div>
+                    <div className="project-name">
+                      Dahabshiil Bank
+                    </div>
+                  </div>
+                </a>
+              </div>
+              <div className="col-lg-4 col-sm-6">
+                <a className="portfolio-box" href="img/portfolio/fullsize/3.jpg" onClick={this.handlePortfolioClick.bind(this, 2)}>
+                  <Img fluid={this.props.data.images.edges[2].node.childImageSharp.fluid}/>
+                  <div className="portfolio-box-caption">
+                    <div className="project-category text-white-50">
+                      Advertisement 
+                    </div>
+                    <div className="project-name">
+                      Darasalam Bank                     </div>
+                  </div>
+                </a>
+              </div>
+              <div className="col-lg-4 col-sm-6">
+                <a className="portfolio-box" href="images/portfolio/fullsize/4.jpg" onClick={this.handlePortfolioClick.bind(this, 3)}>
+                  <Img fluid={this.props.data.images.edges[3].node.childImageSharp.fluid}/>
+                  <div className="portfolio-box-caption">
+                    <div className="project-category text-white-50">
+                      Documentary Production
+                    </div>
+                    <div className="project-name">
+                      Project SWEET
+                    </div>
+                  </div>
+                </a>
+              </div>
+              <div className="col-lg-4 col-sm-6">
+                <a className="portfolio-box" href="img/portfolio/fullsize/5.jpg" onClick={this.handlePortfolioClick.bind(this, 4)}>
+                  <Img fluid={this.props.data.images.edges[4].node.childImageSharp.fluid}/>
+                  <div className="portfolio-box-caption">
+                    <div className="project-category text-white-50">
+                      Branding and Documentary 
+                    </div>
+                    <div className="project-name">
+                      Amoud University Event Branding
+                    </div>
+                  </div>
+                </a>
+              </div>
+              <div className="col-lg-4 col-sm-6">
+                <a className="portfolio-box" href="img/portfolio/fullsize/6.jpg" onClick={this.handlePortfolioClick.bind(this, 5)}>
+                  <Img fluid={this.props.data.images.edges[5].node.childImageSharp.fluid}/>
+                  <div className="portfolio-box-caption p-3">
+                    <div className="project-category text-white-50">
+                      Web Development 
+                    </div>
+                    <div className="project-name">
+                       Double Nexus
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="page-section bg-dark text-white">
+          <div className="container text-center">
+            <h2 className="mb-4"> Do you have a project in mind, Let us know!</h2>
+            <a className="btn btn-light btn-xl" href="https://www.facebook.com/Realvideo.co/" target="_blank">Get Started!</a>
+          </div>
+        </section>
+
+        <section className="page-section" id="contact">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-lg-8 text-center">
+                <h2 className="mt-0">Let's Get In Touch!</h2>
+                <hr className="divider my-4"/>
+                <p className="text-muted mb-5">Ready to start your next project with us? Give us a call or send us an
+                  email
+                  and we will get back to you as soon as possible!</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
+                <i className="fas fa-phone fa-3x mb-3 text-muted"></i>
+                <div>+252 (63) 422-8854</div>
+              </div>
+              <div className="col-lg-4 mr-auto text-center">
+                <i className="fas fa-envelope fa-3x mb-3 text-muted"></i>
+                <a className="d-block" href="mailto:contact@yourwebsite.com">contact@realvideo.com</a>
+              </div>
+            </div>
+          </div>
+        </section>
+        <PortfolioModal show={this.state.modalShow} onHide={() => this.setModal(false, 0)}>
+          <PortfolioCarousel images={this.props.data.images.edges} current={this.state.modalCurrent}/>
+        </PortfolioModal>
+      </Layout>
+    )
+  }
+}
+
+
+export const imageData = graphql`
+  query {
+    images: allFile(filter: {relativePath: {glob: "portfolio/fullsize/*.jpg"}}, sort: {fields: name}) {
+      edges {
+        node {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
